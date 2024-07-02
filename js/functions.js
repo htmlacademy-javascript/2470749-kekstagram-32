662886be5d4d5ff5d4db55d66e55a03cb081f3fd
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-07-02 16:19:03 +0300

    change functions into arrow notation

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,14 @@
 // Функция для проверки длины строки.
 // Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.
 // Эта функция нам пригодится для валидации формы.
-const getStringLength = function (string, maxLength) {
-  return string.length <= maxLength;
-};
+const getStringLength = (string, maxLength) => string.length <= maxLength;
 
 getStringLength('привет', 10);
 
 // Функция для проверки, является ли строка палиндромом.
 // Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 
-const isStringPalindrome = function (string) {
+const isStringPalindrome = (string) => {
   const modifiedString = string.replaceAll(' ', '').toLowerCase();
   let result = 0;
 
