99a05036c6038d898d6ec8b29079cc5fd4c26134
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-07-15 22:14:41 +0300

    fix: refactor result in isMeetingOnWorkingDay function (after code review), convertMinutesInHours

--- js/functions.js
+++ js/functions.js
@@ -52,48 +52,33 @@ const getNumbers = function (string) {
 
 getNumbers('0029 of June 2024 year');
 
-/*
-Напишите функцию, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
-Время указывается в виде строки в формате часы:минуты. Для указания часов и минут могут использоваться как две цифры, так и одна. Например, 8 часов 5 минут могут быть указаны по-разному: 08:05, 8:5, 08:5 или 8:05.
-Продолжительность задаётся числом. Гарантируется, что и рабочий день, и встреча укладываются в одни календарные сутки.
+// функция, которая модифицирует время в число, напр 8:30 = 8.3
+const getModifiedTime = (time) => {
+  const array = time.split(':');
+  const modifiedTime = Number(array.join('.'));
+  return modifiedTime;
+};
 
-'8:00' - начало рабочего дня
-'17:30' - конец рабочего дня
-'14:00' - начало встречи
-90 - продолжительность встречи в минутах
-*/
+// функция, которая переводит минуты в часы, напр 100 минут = 1.4
+const convertMinutesInHours = (minutes) => {
+  const timeInHours = Number(`${Math.floor(minutes / 60)}.${Math.floor(minutes % 60)}`);
+  return timeInHours;
+};
+
 /*
+функция, которая принимает время начала и конца рабочего дня, а также время старта и продолжительность встречи в минутах и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
+для проверки:
 имяФункции('08:00', '17:30', '14:00', 90); // true
-имяФункции('8:0', '10:0', '8:0', 120);     // true
-имяФункции('08:00', '14:30', '14:00', 90); // false
-имяФункции('14:00', '17:30', '08:0', 90);  // false
 имяФункции('8:00', '17:30', '08:00', 900); // false
 */
-
 const isMeetingOnWorkingDay = (startWork, endWork, startMeeting, meetingLength) => {
-  const getModifiedTime = (time) => {
-    const array = time.split(':');
-    const modifiedTime = Number(array.join('.'));
-    return modifiedTime;
-  };
-
-  const convertMinutesInHours = (minutes) => {
-    const array = [Math.floor(minutes / 60), minutes - Math.floor(minutes / 60) * 60];
-    const timeInHours = Number(array.join('.'));
-    return timeInHours;
-  };
-
   const startWorkModified = getModifiedTime(startWork);
   const endWorkModified = getModifiedTime(endWork);
   const endOfMeeting = getModifiedTime(startMeeting) + convertMinutesInHours(meetingLength);
-  let result;
 
-  if (endOfMeeting - startWorkModified >= 0 && endWorkModified - endOfMeeting >= 0) {
-    result = true;
-  } else {
-    result = false;
-  }
+  let result = endOfMeeting - startWorkModified >= 0 && endWorkModified - endOfMeeting >= 0;
+
   return result;
 };
 
-isMeetingOnWorkingDay('08:00', '14:30', '14:00', 90);
+isMeetingOnWorkingDay('08:00', '17:30', '14:00', 90);
