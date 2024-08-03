0040eebc574239076a79aac607ac480077c2edf3
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-08-03 14:53:06 +0300

    fix: add ability not to close modal when the focus on text field when push the ESC, add check comment length

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,16 +1,20 @@
 import { isEscapeKey } from "./util";
 
 const HASHTAGS_REGEXP = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_COMMENTS_LENGTH = 140;
+const MAX_HASHTAGS_COUNT = 5;
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 const uploadInput = document.querySelector('.img-upload__input');
 const closeUploadModalButton = document.querySelector('.img-upload__cancel');
-const hashtags = document.querySelector('.text__hashtags');
-const publishButton = document.querySelector('.img-upload__submit');
+const hashtagField = document.querySelector('.text__hashtags');
+const textCommentField = document.querySelector('.text__description');
+
+const isFieldFocused = () => document.activeElement === textCommentField || document.activeElement === hashtagField;
 
 const onDocumentEscKeyDown = (evt) => {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !isFieldFocused()) {
     evt.preventDefault();
     closeUploadModal();
   }
@@ -43,29 +47,19 @@ closeUploadModalButton.addEventListener('click', (evt) => {
   closeUploadModal();
 });
 
+// валидация:
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
   errorTextClass: 'img-upload__field-wrapper--error',
 });
-
-// ok хэштег начинается с символа # (решётка);
-//ok  строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;
-// ok хеш-тег не может состоять только из одной решётки;
-// ok максимальная длина одного хэштега 20 символов, включая решётку;
-// ok хэштеги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;
-//ok хэштеги разделяются пробелами;
-// один и тот же хэштег не может быть использован дважды;
-// ok нельзя указать больше пяти хэштегов;
-// ok хэштеги необязательны;
-// если фокус находится в поле ввода хэштега, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
-
 
 // проверка количества хэштегов:
 const checkHashtagsArrayLength = (value) => {
   const hashtagsArray = value.trim().split(" ");
 
-  if (hashtagsArray.length === 0 || hashtagsArray.length <= 5) {
+  if (hashtagsArray.length === 0 || hashtagsArray.length <= MAX_HASHTAGS_COUNT) {
     return true;
   } else {
     return false;
@@ -75,7 +69,6 @@ const checkHashtagsArrayLength = (value) => {
 // проверка повторяющихся хэштегов:
 const checkHashtagsRepeat = (value) => {
   const hashtagsArray = value.trim().split(" ");
-  console.log(hashtagsArray);
 
   const modifiedHashtagArray = hashtagsArray.map((hashtag) => {
     let modifiedHashtag = hashtag.replaceAll(' ', '').toLowerCase();
@@ -86,7 +79,7 @@ const checkHashtagsRepeat = (value) => {
     if (modifiedHashtagArray.length === 1) {
       return true;
     } else if (modifiedHashtagArray.every((element) => element !== modifiedHashtagArray[i])) {
-     return true;
+      return true;
     } else {
       return false;
     };
@@ -96,15 +89,29 @@ const checkHashtagsRepeat = (value) => {
 // проверка корректности введения символов хэштега:
 const checkHashtagsRegister = (value) => {
   const hashtagsArray = value.trim().split(' ');
-  console.log(hashtagsArray);
-  return hashtagsArray.every((element) => HASHTAGS_REGEXP.test(element));
+
+  if (value === '') {
+    return true;
+  } else {
+    return hashtagsArray.every((element) => HASHTAGS_REGEXP.test(element));
+  }
 };
 
-pristine.addValidator(hashtags, checkHashtagsArrayLength, 'Максимум 5 хэштегов');
-pristine.addValidator(hashtags, checkHashtagsRepeat, 'Выявлены повторяющиеся хэштеги');
-pristine.addValidator(hashtags, checkHashtagsRegister, 'Хэштег содержит только # или введены недопустимые символы или длина хэштега превышает 20 символов или хэштеги должны разделяться пробелом');
+// проверка длины комментария:
+const checkCommentLength = (value) => value.length <= MAX_COMMENTS_LENGTH;
 
+pristine.addValidator(hashtagField, checkHashtagsArrayLength, 'Максимум 5 хэштегов');
+pristine.addValidator(hashtagField, checkHashtagsRepeat, 'Выявлены повторяющиеся хэштеги');
+pristine.addValidator(hashtagField, checkHashtagsRegister, 'Введены недопустимые символы');
+pristine.addValidator(textCommentField, checkCommentLength, 'Длина комментария не более 140 символов')
+
+// отправка формы, если все данные введены корректно:
 uploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  pristine.validate();
+  if (pristine.validate()) {
+    evt.target.submit();
+  }
 });
+
+
+
