0d392f06f59c57213b330a323c5d98eed7002a43
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-08-08 15:10:47 +0300

    feat: add functionality: receiving data from the server, showing data loading errors

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-import { getPhotosDataArray } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import './fullsize.js';
 import './upload-form.js';
 import './filter.js';
 
-getPhotosDataArray();
-renderThumbnails();
+const showError = () => {
+  const dataErrorMessage = document.querySelector('#data-error').content.querySelector('.data-error');
+  document.body.appendChild(dataErrorMessage);
+
+  setTimeout(function(){
+    dataErrorMessage.remove();
+  }, 5000);
+}
+
+fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((picturesData) => {
+    renderThumbnails(picturesData);
+  })
+  .catch(() => {
+    showError();
+  });
